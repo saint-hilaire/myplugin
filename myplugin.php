18c022ab1009689676f882a5f383a8018c7d9a15
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2021-12-05 12:08:26 +0100

    Initial commit

---
+++ myplugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: My Plugin
+Plugin URI: https://example.com
+Description: My Plugin
+Version: 0.1.0
+Author: Me
+Author URI: http://myplugin.example.com
+Text Domain: myplugin
+Domain Path: /languages
+ */
+die("My Plugin was here.");
